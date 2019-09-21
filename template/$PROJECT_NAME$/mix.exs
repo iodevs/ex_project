fcd7691b5559bfb90c89c34314d2194333362e34
Jindrich K. Smitka <jsmitka@smita.info>
2019-09-21 12:00:05 +0200

    Updated dependencies for generated project

--- template/$PROJECT_NAME$/mix.exs
+++ template/$PROJECT_NAME$/mix.exs
@@ -36,9 +36,9 @@ defmodule <%= @project_name_camel_case %>.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:ex_doc, "~> 0.18.1", only: :dev},
-      {:credo, "~> 0.9", only: [:dev, :test]},
-      {:excoveralls, "~> 0.9", only: :test},
+      # {:ex_doc, "~> 0.21", only: :dev},
+      {:credo, "~> 1.1", only: [:dev, :test]},
+      {:excoveralls, "~> 0.11", only: :test},
     ]
   end
 end
