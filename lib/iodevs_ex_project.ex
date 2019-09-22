132acc25924ac2718d802be0439b2cb5355f6798
Jindrich K. Smitka <jsmitka@smita.info>
2019-09-22 08:12:58 +0200

    Added dialyzer

--- lib/iodevs_ex_project.ex
+++ lib/iodevs_ex_project.ex
@@ -6,6 +6,15 @@ defmodule IodevsExProject do
     name:       :iodevs_ex_project,
     short_desc: "A iodevs elixir project template",
     source_dir: "../template",
-    based_on: :project
+    based_on: :project,
+    options: [
+      dia: [
+        to: :dialyzer,
+        # default: nil,
+        takes: "ci_platform",
+        desc: "append dialyzer settings (default: nil). Posible values is base, semaphore."
+      ],
+      dialyzer: [same_as: :dia]
+    ]
 
 end
--- template/$PROJECT_NAME$/mix.exs
+++ template/$PROJECT_NAME$/mix.exs
@@ -4,6 +4,22 @@ defmodule <%= @project_name_camel_case %>.MixProject do
   def project do
     [
       app: :<%= @project_name %>,
+<%= if @dialyzer == "base" do %>
+      dialyzer: [
+        plt_add_apps: [:mix],
+        plt_add_deps: :transitive,
+        ignore_warnings: "dialyzer.ignore-warnings",
+        flags: [
+          :unmatched_returns,
+          :error_handling,
+          :race_conditions,
+          :no_opaque
+        ]
+      ],
+<% end %>
+<%= if @dialyzer == "semaphore" do %>
+      dialyzer: dialyzer_base() |> dialyzer_ptl(System.get_env("SEMAPHORE_CACHE_DIR")),
+<% end %>
       version: "0.1.0",
       elixir: "~> <%= @elixir_version |> String.split(".") |> Enum.take(2) |> Enum.join(".") %>",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -39,6 +55,60 @@ defmodule <%= @project_name_camel_case %>.MixProject do
       # {:ex_doc, "~> 0.21", only: :dev},
       {:credo, "~> 1.1", only: [:dev, :test]},
       {:excoveralls, "~> 0.11", only: :test},
+<%= if @dialyzer do %>
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
+<%end %>
     ]
   end
+
+<%= if @dialyzer == "semaphore" do %>
+  defp dialyzer_base() do
+    [
+      plt_add_apps: [:mix],
+      plt_add_deps: :transitive,
+      ignore_warnings: "dialyzer.ignore-warnings",
+      flags: [
+        :unmatched_returns,
+        :error_handling,
+        :race_conditions,
+        :no_opaque
+      ]
+    ]
+  end
+
+  defp dialyzer_ptl(base, nil) do
+    base
+  end
+
+  defp dialyzer_ptl(base, path) do
+    base ++
+      [
+        plt_core_path: path,
+        plt_file:
+          Path.join(
+            path,
+            "dialyxir_erlang-#{otp_vsn()}_elixir-#{System.version()}_deps-dev.plt"
+          )
+      ]
+  end
+
+  defp otp_vsn() do
+    major = :erlang.system_info(:otp_release) |> List.to_string()
+    vsn_file = Path.join([:code.root_dir(), "releases", major, "OTP_VERSION"])
+
+    try do
+      {:ok, contents} = File.read(vsn_file)
+      String.split(contents, "\n", trim: true)
+    else
+      [full] ->
+        full
+
+      _ ->
+        major
+    catch
+      :error, _ ->
+        major
+    end
+  end
+<% end %>
 end
